d9b55ea4dad42587e07d990db521aba5211e404e
Sean R Reid <sean@torchcodelab.com>
2019-11-15 13:04:18 -0500

    Added form to submit a comment, and function to pass data to a POST route

--- src/components/blogPost.js
+++ src/components/blogPost.js
@@ -1,32 +1,102 @@
 import React, { Component } from "react";
+import { loadData } from "../utils/loadData";
 
 class BlogPost extends Component {
   state = {
-    post: {}
+    post: {},
+    comments: [],
+    username: "",
+    comment: ""
   };
 
   async componentDidMount() {
-    const post = await this.loadData();
+    const postId = this.props.match.params.post_id;
+    const post_url = `http://localhost:3000/v1/post/${postId}`;
+    const comment_url = `http://localhost:3000/v1/post/comments/${postId}`;
+
+    const post = await loadData(post_url);
+    const comments = await loadData(comment_url);
+
     this.setState({
-      post
+      post,
+      comments
     });
   }
 
-  loadData = async () => {
-    const postId = this.props.match.params.post_id;
-    const url = `http://localhost:3000/v1/post/${postId}`;
-    const response = await fetch(url);
-    const data = response.json();
-    return data;
+  handleChange = e => {
+    const { name, value } = e.target;
+
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { username, comment } = this.state;
+    const post_id = this.props.match.params.post_id;
+
+    const data = { post_id, username, comment };
+
+    this.addComment(data);
+  };
+
+  addComment = async data => {
+    const response = await fetch("http://localhost:3000/v1/post/add/comment", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+    const reply = await response;
+    if (reply.status === 200) {
+      alert("Comment Saved!");
+    }
+    if (reply.status !== 200) {
+      alert("womp womp");
+    }
   };
 
   render() {
-    console.log(this.props);
-    const { post } = this.state;
+    const { post, name, comment, comments } = this.state;
     return (
       <div>
         <h2>{post.title}</h2>
         <p>{post.content}</p>
+        <div className="comment-form">
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Your Name
+              <input
+                type="text"
+                value={name}
+                placeholder="Your Name"
+                name="username"
+                onChange={this.handleChange}
+              />
+            </label>
+            <label>
+              Your Comment:
+              <textarea
+                name="comment"
+                value={comment}
+                placeholder="Your Comment"
+                onChange={this.handleChange}
+              />
+              <button type="submit">Submit</button>
+            </label>
+          </form>
+          <ul>
+            {comments.map(comment => (
+              <li key={comment.id}>
+                <h3>{comment.username} says:</h3>
+                <p>{comment.comment}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
